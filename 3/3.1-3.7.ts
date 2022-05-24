3c3ed5ef9a3cde16a8df6fbe9c3aa1d9eb7ab0ea
angrasan <a0n9g2r6a@outlook.jp>
2022-05-24 23:21:40 +0900

    radian

--- 3/3.1-3.7.ts
+++ 3/3.1-3.7.ts
@@ -1,3 +1,21 @@
-let message: string = 'Hello TypeScript';
+let degree: number = 160;
+let radian: number = degToRad(degree);
+console.log({radian});
 
-console.log({message});
+// function degToRad(degree: number): number {
+//   return (degree / 360) * (Math.PI * 2);
+// }
+function degToRad(degree: number): number {
+  return degree * (Math.PI / 180);
+}
+
+let sin: number = Math.sin(radian);
+let cos: number = Math.cos(radian);
+console.log({sin});
+console.log({cos});
+
+// 座標A => degree方向にr分移動した座標Bを求める
+let r: number = 3;
+let A: number[] = [1.0, -3.0];
+let B: number[] = [A[0] + cos * r, A[1] + sin * r];
+console.log({B})
