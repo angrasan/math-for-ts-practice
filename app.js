047923e15e06261a6a112c6a6e146a43acb67f9f
angrasan <a0n9g2r6a@outlook.jp>
2022-05-29 00:55:03 +0900

    create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+var canvas = document.querySelector('#main_canvas');
+var context = canvas.getContext('2d');
+context.fillRect(0, 0, 100, 100);
+console.log(canvas);
+console.log('Hello TypeScript');
--- app.ts
+++ app.ts
@@ -3,4 +3,4 @@ let context = canvas.getContext('2d') as CanvasRenderingContext2D;
 
 context.fillRect(0, 0, 100, 100);
 console.log(canvas);
-console.log('Hello JavaScript');
+console.log('Hello TypeScript');
